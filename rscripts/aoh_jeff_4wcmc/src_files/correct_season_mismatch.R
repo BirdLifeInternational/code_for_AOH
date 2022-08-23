a5f57929b5826d9c15e0b9ade8078ce086ffe1f2
michebio <michebusana@gmail.com>
2022-08-23 23:20:52 +0200

    rewritten as function

--- rscripts/aoh_jeff_4wcmc/src_files/correct_season_mismatch.R
+++ rscripts/aoh_jeff_4wcmc/src_files/correct_season_mismatch.R
@@ -10,35 +10,37 @@
 # tmp <- spp_range_data[, "seasonal", drop= F]
 # tmp <- sf::st_drop_geometry(tmp)
 # tmp <- merge(tmp, hb_iucn_code, by.x = "seasonal", by.y = "code", all.x = T)
-
-if(any(spp_habitat_data$season == "Breeding Season")){
-  tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Breeding Season")
-  tmp$season <- "Resident"
-  tmp2$season <- "Seasonal Occurrence Unknown"
-  spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2)
-} 
-
-if(any(spp_habitat_data$season == "Non-Breeding Season")){
-  tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Non-Breeding Season")
-  tmp$season <- "Resident"
-  tmp2$season <- "Seasonal Occurrence Unknown"
-  spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2)
-} 
-
-if(any(spp_habitat_data$season == "Seasonal Occurrence Unknown")){
-  tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Seasonal Occurrence Unknown")
-  tmp$season <- "Resident"
-  tmp2$season <- "Breeding Season"
-  tmp3$season <- "Non-Breeding Season"
-  spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2, tmp3)
-} 
-
-if(any(spp_habitat_data$season == "Resident")){
-  tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Resident")
-  tmp$season <- "Breeding Season"
-  tmp2$season <- "Seasonal Occurrence Unknown"
-  tmp3$season <- "Non-Breeding Season"
-  spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2, tmp3)
-} 
-
-spp_habitat_data <- unique(spp_habitat_data)
+correct_season_mismatch <- function(spp_habitat_data){
+  if(any(spp_habitat_data$season == "Breeding Season")){
+    tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Breeding Season")
+    tmp$season <- "Resident"
+    tmp2$season <- "Seasonal Occurrence Unknown"
+    spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2)
+  } 
+  
+  if(any(spp_habitat_data$season == "Non-Breeding Season")){
+    tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Non-Breeding Season")
+    tmp$season <- "Resident"
+    tmp2$season <- "Seasonal Occurrence Unknown"
+    spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2)
+  } 
+  
+  if(any(spp_habitat_data$season == "Seasonal Occurrence Unknown")){
+    tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Seasonal Occurrence Unknown")
+    tmp$season <- "Resident"
+    tmp2$season <- "Breeding Season"
+    tmp3$season <- "Non-Breeding Season"
+    spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2, tmp3)
+  } 
+  
+  if(any(spp_habitat_data$season == "Resident")){
+    tmp <- tmp2 <- tmp3 <- subset(spp_habitat_data, season == "Resident")
+    tmp$season <- "Breeding Season"
+    tmp2$season <- "Seasonal Occurrence Unknown"
+    tmp3$season <- "Non-Breeding Season"
+    spp_habitat_data <- rbind(spp_habitat_data, tmp, tmp2, tmp3)
+  } 
+  
+  spp_habitat_data <- unique(spp_habitat_data)
+  return(spp_habitat_data)
+}
