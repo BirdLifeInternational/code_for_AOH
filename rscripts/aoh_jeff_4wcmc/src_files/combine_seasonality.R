6abb9a4770b087dfa59e011a112919250fc9284d
michebio <michebusana@gmail.com>
2022-08-29 23:50:41 +0200

    modify the way files are written and renamed with seasonality

--- rscripts/aoh_jeff_4wcmc/src_files/combine_seasonality.R
+++ rscripts/aoh_jeff_4wcmc/src_files/combine_seasonality.R
@@ -20,78 +20,41 @@ combine_seasonality <- function(check_combo = check_combo,
     fwrite(data.table(msg = msg), file = paste0(output_errors, "msg_", i, ".csv"))
     not_processed_species[[paste0(taxon_id)]] <- msg
   } else if(str_length(check_combo) == 1){
+    if(text_file != "Breeding_and_Nonbreeding"){
     target_file <- files_aoh[str_detect(files_aoh, paste0("_", check_combo, ".tif$"))]
-    #file.rename(target_file, paste0(updated_path, taxon_id, "_", text_file, ".tif"))
-    one <- rast(target_file)
-    writeRaster(one, paste0(updated_path, taxon_id, "_", text_file, ".tif"), 
-                  overwrite = TRUE)
+    file.rename(target_file, paste0(updated_path, taxon_id, "_", text_file, ".tif"))
+    } else if(text_file == "Breeding_and_Nonbreeding"){
+    	msg <- paste0("Taxon_id ", taxon_id, 
+    	" is migratory; but the only seasonality in check_combo is: ", check_combo, ". Why?")
+    	print(msg)
+    	fwrite(data.table(msg = msg), file = paste0(output_errors, "msg_", i, "_within_combine_seasonality.csv"))
+    }
+    # one <- terra::rast(target_file)
+    # terra::writeRaster(one, paste0(updated_path, taxon_id, "_", text_file, ".tif"), 
+    #               overwrite = TRUE)
   } else if(str_length(check_combo) == 2) {
-    one <- str_sub(check_combo, 1, 1)
-    two <- str_sub(check_combo, 2, 2)
-    target_file_1 <- files_aoh[str_detect(files_aoh, paste0("_", one, ".tif$"))]
-    target_file_2 <- files_aoh[str_detect(files_aoh, paste0("_", two, ".tif$"))]
-    one <- rast(target_file_1)
-    two <- rast(target_file_2)
-    rlist <- list(one, two)
-    rsrc <- sprc(rlist)
-    mer <- merge(rsrc, filename = paste0(updated_path, taxon_id, "_", text_file, ".tif"), overwrite = T)
-    # if(type == "LR"){
-    #   print("the values of the merged file are between:")
-    #   print(unique(values(mer)))
-    # }
-    rm(one, two, rlist, rsrc, target_file_1, target_file_2)
-  } else if(str_length(check_combo) == 3) {
-    one <- str_sub(check_combo, 1, 1)
-    two <- str_sub(check_combo, 2, 2)
-    three <- str_sub(check_combo, 3, 3)
-    target_file_1 <- files_aoh[str_detect(files_aoh, paste0("_", one, ".tif$"))]
-    target_file_2 <- files_aoh[str_detect(files_aoh, paste0("_", two, ".tif$"))]
-    target_file_3 <- files_aoh[str_detect(files_aoh, paste0("_", three, ".tif$"))]
-    one <- rast(target_file_1)
-    two <- rast(target_file_2)
-    three <- rast(target_file_3)
-    rlist <- list(one, two, three)
-    rsrc <- sprc(rlist)
-    mer <- merge(rsrc, filename = paste0(updated_path, taxon_id, "_", text_file, ".tif"), overwrite = T)
-    rm(one, two, three, rlist, rsrc, target_file_1, target_file_2, target_file_3)
-  } else if(str_length(check_combo) == 4) {
-    one <- str_sub(check_combo, 1, 1)
-    two <- str_sub(check_combo, 2, 2)
-    three <- str_sub(check_combo, 3, 3)
-    four <- str_sub(check_combo, 4, 4)
-    target_file_1 <- files_aoh[str_detect(files_aoh, paste0("_", one, ".tif$"))]
-    target_file_2 <- files_aoh[str_detect(files_aoh, paste0("_", two, ".tif$"))]
-    target_file_3 <- files_aoh[str_detect(files_aoh, paste0("_", three, ".tif$"))]
-    target_file_4 <- files_aoh[str_detect(files_aoh, paste0("_", four, ".tif$"))]
-    one <- rast(target_file_1)
-    two <- rast(target_file_2)
-    three <- rast(target_file_3)
-    four <- rast(target_file_4)
-    rlist <- list(one, two, three, four)
-    rsrc <- sprc(rlist)
-    mer <- merge(rsrc, filename = paste0(updated_path, taxon_id, "_", text_file, ".tif"), overwrite = T)
-    rm(one, two, three, four, rlist, rsrc, target_file_1, target_file_2, target_file_3, target_file_4)
-  } else if(str_length(check_combo) == 5) {
-    one <- str_sub(check_combo, 1, 1)
-    two <- str_sub(check_combo, 2, 2)
-    three <- str_sub(check_combo, 3, 3)
-    four <- str_sub(check_combo, 4, 4)
-    five <- str_sub(check_combo, 5, 5)
-    target_file_1 <- files_aoh[str_detect(files_aoh, paste0("_", one, ".tif$"))]
-    target_file_2 <- files_aoh[str_detect(files_aoh, paste0("_", two, ".tif$"))]
-    target_file_3 <- files_aoh[str_detect(files_aoh, paste0("_", three, ".tif$"))]
-    target_file_4 <- files_aoh[str_detect(files_aoh, paste0("_", four, ".tif$"))]
-    target_file_5 <- files_aoh[str_detect(files_aoh, paste0("_", five, ".tif$"))]
-    one <- rast(target_file_1)
-    two <- rast(target_file_2)
-    three <- rast(target_file_3)
-    four <- rast(target_file_4)
-    five <- rast(target_file_5)
-    rlist <- list(one, two, three, four, five)
-    rsrc <- sprc(rlist)
-    mer <- merge(rsrc, filename = paste0(updated_path, taxon_id, "_", text_file, ".tif"), overwrite = T)
-    rm(one, two, three, four, five, rlist, rsrc, 
-       target_file_1, target_file_2, target_file_3, target_file_4, target_file_5)
+    if(text_file == "Breeding_and_Nonbreeding"){
+      spp_aoh$seasonal <- 23
+      both_bred_nonb <- spp_aoh %>%  group_by(seasonal) %>%
+        summarise(geometry = sf::st_union(geometry), 
+                  sci_name = unique(spp_range_data$sci_name),
+                  id_no = unique(spp_range_data$id_no),
+                  seasonal = 23
+        ) %>% ungroup()
+      
+      both_bred_nonb <- stars::st_rasterize(both_bred_nonb %>% dplyr::select(id_no, geometry))
+      
+      # export as tiff
+      stars::write_stars(both_bred_nonb, 
+                         dsn = paste0(updated_path, taxon_id, "_", text_file, ".tif"),
+                         normalize_path = TRUE)
+      
+    } else {
+      stop("two seasonal polygons from spp_aoh but the type is not breeding+nonbreeding")
+    }
+
+  } else if(str_length(check_combo) >= 3) {
+    stop("3+ seasonal polygons")
   }
   prt_msg <- paste0("Written_raster_file_", paste0(updated_path, taxon_id, "_", text_file))
   prt_msg <- str_replace_all(prt_msg, "\\/", "_")
