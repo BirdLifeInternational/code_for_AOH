01442cd925cfee0c5bfe00418e483beefe6fe6ef
michebio <michebusana@gmail.com>
2022-11-28 13:05:02 +0100

    use gdal

--- rscripts/aoh_jeff_4wcmc/src_files/calculate_area.R
+++ rscripts/aoh_jeff_4wcmc/src_files/calculate_area.R
@@ -1,27 +1,18 @@
-# calculate area of raster by simply counting how many cells are suitable or not
-
-calculate_area <- function(type, wd_path, updated_path, output_area, taxon_id, text_file, spp_summary_data, check_combo){
-  #### add also to breeding and non-breeding#####
-  if(str_detect(type, "LR")){
-    target_file <- paste0(updated_path, taxon_id, "_", text_file, ".tif")
-    if(file.exists(target_file)){
-      x <- terra::rast(target_file)
-      freqs <- terra::freq(x)
-      count0 <- subset(freqs, value == 0)$count
-      count1 <- subset(freqs, value == 1)$count
-      
-      area <- data.table(id_no = taxon_id, scientific_name = unique(spp_summary_data$binomial),
-                         bird_or_mammal = unique(spp_summary_data$bird_or_mammal),
-                         total_num_pix = (count0 + count1), 
-                         suitable_num_pix = count1,
-                         not_suitable_num_pix = count0,
-                         type = text_file,
-                         seasonality_combination = check_combo,
-                         raster_name = paste(target_file))
-      fwrite(area, file = paste0(output_area, "suitable_pixel_calculation_", taxon_id, "_", text_file, ".csv"),
-             append = FALSE)
-    }
-  }
-  return(0)
+# calculate frequencies of values within a raster
+library(gdalUtilities)
+library(stringr)
+#file_nam = "/home/michela/Data/git/Birdlife/aoh_out/output_R/birds/lumbierres_LR/aoh_tif/22714939_3.tif"
+frequency_gdalinfo_raster <- function(file_nam){
+  file_nam <- normalizePath(file_nam)
+  gdalLog <- capture.output(gdalUtilities::gdalinfo(file_nam, hist = TRUE, dryrun=FALSE))
+  area_calc <- gdalLog[which(stringr::str_detect(gdalLog, "buckets from")) + 1]
+  area_calc <- strsplit(area_calc, "\\s+")[[1]]
+  if(area_calc[1] == "") area_calc <- area_calc[2:length(area_calc)]
+  if(any(area_calc == "")) print("some empty values here")
+  
+  area_calc <- as.numeric(area_calc)
+  vals <- 0:(length(area_calc)-1)
+  summa <- data.table::data.table(code_values = vals, frequency = area_calc)
+  unlink(paste0(file_nam, ".aux.xml"), recursive = TRUE)
+  return(summa)
 }
-
--- rscripts/aoh_jeff_4wcmc/src_files/combine_seasonality.R
+++ rscripts/aoh_jeff_4wcmc/src_files/combine_seasonality.R
@@ -13,56 +13,138 @@ combine_seasonality <- function(check_combo = check_combo,
                                 type = type,
                                 output_errors = output_errors, 
                                 spp_summary_data = spp_summary_data,
-                                output_area = output_area){
+                                output_area = output_area,
+                                output_df = output_df,
+                                spp_aoh = spp_aoh){
+  
+  prt_msg <- paste0("Processed ", paste0(updated_path, taxon_id, "_", text_file))
+  prt_msg <- str_replace_all(prt_msg, "\\/", "_")
 
   if(str_length(check_combo) == 0){
     msg <- paste0("Species ", taxon_id, " does not have any polygon to draw the breeding AOH")
-    fwrite(data.table(msg = msg), file = paste0(output_errors, "msg_", i, ".csv"))
+    fwrite(data.table(msg = msg, taxon_id = taxon_id), file = paste0(output_errors, "msg_", i, ".csv"))
     not_processed_species[[paste0(taxon_id)]] <- msg
   } else if(str_length(check_combo) == 1){
     if(text_file != "Breeding_and_Nonbreeding"){
     target_file <- files_aoh[str_detect(files_aoh, paste0("_", check_combo, ".tif$"))]
-    file.rename(target_file, paste0(updated_path, taxon_id, "_", text_file, ".tif"))
+    # make a copy of original file in different directory
+    R.utils::copyFile(target_file, output_aoh_tif, skip = FALSE, overwrite=TRUE, 
+    	validate=TRUE,  verbose=FALSE)
+    target_file <- str_replace_all(target_file, "\\/\\/", "\\/")
+    file_nam <- paste0(updated_path, taxon_id, "_", text_file, ".tif")
+    file_nam <- str_replace_all(file_nam, "\\/\\/", "\\/")
+    if(file.exists(file_nam)) unlink(file_nam, recursive = T)
+    #file.rename(target_file, file_nam)
+    R.utils::renameFile(target_file, file_nam, overwrite=TRUE, verbose=TRUE)
     } else if(text_file == "Breeding_and_Nonbreeding"){
     	msg <- paste0("Taxon_id ", taxon_id, 
-    	" is migratory; but the only seasonality in check_combo is: ", check_combo, ". Why?")
+    	" is migratory; but the only seasonality in check_combo is: ", check_combo)
     	print(msg)
     	fwrite(data.table(msg = msg), file = paste0(output_errors, "msg_", i, "_within_combine_seasonality.csv"))
+    	
+    	files_in_copy <- list.files(normalizePath(output_aoh_tif), pattern = paste0(taxon_id, "_"), full.names =TRUE)
+    	target_file <- files_in_copy[1]
+    	# make a copy of original file in different directory
+    	target_file <- str_replace_all(target_file, "\\/\\/", "\\/")
+    	file_nam <- paste0(normalizePath(updated_path), "/", taxon_id, "_", text_file, ".tif")
+    	file_nam <- str_replace_all(file_nam, "\\/\\/", "\\/")
+    	if(file.exists(file_nam)) unlink(file_nam, recursive = T)
+    	R.utils::renameFile(target_file, file_nam, overwrite=TRUE, verbose=TRUE)
     }
+    print(prt_msg)
     # one <- terra::rast(target_file)
     # terra::writeRaster(one, paste0(updated_path, taxon_id, "_", text_file, ".tif"), 
     #               overwrite = TRUE)
   } else if(str_length(check_combo) == 2) {
     if(text_file == "Breeding_and_Nonbreeding"){
-      spp_aoh$seasonal <- 23
-      both_bred_nonb <- spp_aoh %>%  group_by(seasonal) %>%
-        summarise(geometry = sf::st_union(geometry), 
-                  sci_name = unique(spp_range_data$sci_name),
-                  id_no = unique(spp_range_data$id_no),
-                  seasonal = 23
-        ) %>% ungroup()
+     
+      file_nam <- paste0(updated_path, taxon_id, "_Breeding.tif")
+      file_nam <- str_replace_all(file_nam, "\\/\\/", "\\/")
+      if(!file.exists(file_nam)) print("breeding tif missing")
+      x <- file_nam
+
+      file_nam <- paste0(updated_path, taxon_id, "_Nonbreeding.tif")
+      file_nam <- str_replace_all(file_nam, "\\/\\/", "\\/")
+      if(!file.exists(file_nam)) print("non_breeding tif missing")
+      y <- file_nam
+     
+      file_nam <- paste0(updated_path, taxon_id, "_", text_file, "_tocompress.tif")
+      #print(paste0("file_nam is ", file_nam))
+      if(file.exists(file_nam)) unlink(file_nam, recursive = T)
+      if(file.exists(file_nam)) unlink(file_nam)
+
+      gdalUtilities::gdalwarp(dstfile = file_nam, srcfile = c(x, y),
+                              t_srs = "ESRI:54017", 
+                              r = "max", of = "GTiff",
+                              ot = "Byte",
+                              dryrun = F)
+ 
+      compressed_file_nam <- paste0(updated_path, taxon_id, "_", text_file, ".tif")
+      print(paste0("file_nam is ", compressed_file_nam))
+      if(file.exists(compressed_file_nam)) unlink(compressed_file_nam, recursive = T)
       
-      both_bred_nonb <- stars::st_rasterize(both_bred_nonb %>% dplyr::select(id_no, geometry))
+      # writeRaster(rast(file_nam), 
+      #             paste0(updated_path, taxon_id, "_", text_file, "_test.tif"),  
+      #             gdal=c("COMPRESS=LZW"))
+      gdalUtilities::gdal_translate(src_dataset = file_nam,
+                                    dst_dataset = compressed_file_nam,
+                                    co = "COMPRESS=LZW", #ot = "Byte",
+                                    dryrun = F)
       
-      # export as tiff
-      stars::write_stars(both_bred_nonb, 
-                         dsn = paste0(updated_path, taxon_id, "_", text_file, ".tif"),
-                         normalize_path = TRUE)
+      if(file.exists(file_nam)) unlink(file_nam, recursive = T)
+      if(file.exists(file_nam)) unlink(file_nam)
+
+      print(paste0("combined seasonalities for ", taxon_id))
       
     } else {
-      stop("two seasonal polygons from spp_aoh but the type is not breeding+nonbreeding")
+      print("two seasonal polygons from spp_aoh but the type is not breeding+nonbreeding")
     }
 
   } else if(str_length(check_combo) >= 3) {
-    stop("3+ seasonal polygons")
+    print("3+ seasonal polygons")
   }
-  prt_msg <- paste0("Written_raster_file_", paste0(updated_path, taxon_id, "_", text_file))
-  prt_msg <- str_replace_all(prt_msg, "\\/", "_")
-  #print(prt_msg)
-  fwrite(data.table(taxon_id = taxon_id), file = paste0(output_errors, prt_msg, ".csv"))
-  calculate_area(type = type, wd_path = wd_path, updated_path = updated_path,
-                 output_area = output_area,
-                 taxon_id = taxon_id, text_file = text_file,
-                 spp_summary_data = spp_summary_data, check_combo = check_combo)
+  
+  
+  if(stringr::str_detect(type, "LR")){
+    area_calc <- frequency_gdalinfo_raster(file_nam)
+  
+    freq_0 <- area_calc[code_values == 0, "frequency"]
+    freq_1 <- area_calc[code_values == 1, "frequency"]
+    short_nam = str_remove(file_nam, updated_path)
+    area_dt <- data.table(raster_name = short_nam, unsuitable = freq_0, suitable = freq_1, taxon_id = taxon_id,
+               seasonality = text_file, type = "LR")
+    fwrite(area_dt, file = paste0(output_area, taxon_id, "_", text_file, ".csv"))
+    
+    print("area calc successful")
+  }
+  # check habitat codes within the unsuitable pixels for those aoh clipped by habitat only 
+  # and with low model prevalence
+  if(
+    (model_validation && habitat_only && ((freq_1/(freq_0 + freq_1)) < 0.25) ) |
+     (!model_validation && str_detect(type, "LR") &&
+      unique(spp_summary_data$elevation_from_iucn) == "no" &&
+      ((freq_1/(freq_0 + freq_1)) < 0.25))
+     ){
+    summarize_habitat_code_unsuitable_pixels(
+             updated_path = normalizePath(updated_path),
+             taxon_id = taxon_id,
+             crosswalk = crosswalk_data,
+             path_habitat_rast = normalizePath(path_habitat_tif),
+             output_df = normalizePath(output_df),
+             model_validation = model_validation,
+             habitat_only = habitat_only,
+             file_nam = file_nam,
+             text_file = text_file # seasonality combination
+             )
+    print("checking unsuitable habitat codes successful")
+  }
   return(0)
 }
+
+
+
+
+
+
+
+
